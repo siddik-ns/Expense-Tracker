22d29a0f98fb03b89ede903268c6323ee9c528a3
siddik-ns <siddik@namespaceit.com>
2021-03-14 16:30:49 +0600

    Unnecessary files have been removed from the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React App</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, facilis, praesentium. Aperiam assumenda cumque eos in ipsum quia quo reiciendis saepe tempora totam. Animi cumque dolore necessitatibus, quae repellat totam!</p>
     </div>
   );
 }
