7e0b42c77c4753575b2b2bb5133b55fbbe3d5e72
siddik-ns <siddik@namespaceit.com>
2021-03-16 19:27:35 +0600

    working speechly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import {Grid} from "@material-ui/core";
 import Details from "./components/Details/Details";
 import Main from "./components/Main/Main";
-import useStyles from './styles'
+import useStyles from './styles';
+import {PushToTalkButton, PushToTalkButtonContainer} from "@speechly/react-ui";
 
 function App() {
   const classes = useStyles();
@@ -19,6 +20,9 @@ function App() {
           <Details title="Expense"/>
         </Grid>
       </Grid>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton/>
+      </PushToTalkButtonContainer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "./context/context";
+import {SpeechProvider} from "@speechly/react-client";
 
 ReactDOM.render(
+  <SpeechProvider appId="99774cdc-e990-4c7a-9757-227da021f3ac" language="en-US">
     <Provider>
       <App />
-    </Provider>,
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 );
